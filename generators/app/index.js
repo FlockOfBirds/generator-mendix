ad4ccdcb48f5c60eb12bdff5d70d7d04d8b78ef9
Joseph D <j_seremba@live.com>
2018-08-03 09:59:38 +0300

    Process review feedback

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,22 +1,24 @@
 /*jshint -W108,-W069*/
-'use strict';
+"use strict";
 
-const pkg = require(__dirname + '/../../package.json');
-const fs = require('fs');
-const extfs = require('extfs');
+const pkg = require(__dirname + "/../../package.json");
+const fs = require("fs");
+const extfs = require("extfs");
 const xml2js = require("xml2js");
 const parser = new xml2js.Parser();
-const Generator = require('yeoman-generator');
+const Generator = require("yeoman-generator");
 
-const promptTexts = require('./lib/prompttexts.js');
-const text = require('./lib/text.js');
+const promptTexts = require("./lib/prompttexts.js");
+const text = require("./lib/text.js");
 
-const boilerPlatePath = 'BadgeWidgetBoilerplate/';
+const boilerPlatePath = "BadgeWidgetBoilerplate/",
+  emptyBoilerplatePath = "EmptyWidgetBoilerplate/";
 
 const banner = text.getBanner(pkg);
+const widgetSrcFolder = "src/components/";
 
 module.exports = class extends Generator {
-  constructor(args, opts) {
+  constructor (args, opts) {
     super(args, opts);
   }
 
@@ -46,8 +48,7 @@ module.exports = class extends Generator {
           this.current.author = destPkg.author;
           this.current.copyright = destPkg.copyright;
           this.current.license = destPkg.license;
-          this.current.builder =
-            typeof destPkg.devDependencies.grunt !== "undefined"
+          this.current.builder = typeof destPkg.devDependencies.grunt !== "undefined"
               ? "grunt"
               : "gulp";
         } catch (e) {
@@ -65,7 +66,7 @@ module.exports = class extends Generator {
           .toString();
         parser.parseString(
           pkgXml,
-          function(err, result) {
+          function (err, result) {
             if (err) {
               this.log("Error: " + err);
               this.FINISHED = true;
@@ -108,14 +109,14 @@ module.exports = class extends Generator {
 
     if (this.isNew) {
       this.prompt(promptTexts.promptsNew()).then(
-        function(props) {
+        function (props) {
           this.props = props;
           done();
         }.bind(this)
       );
     } else {
       this.prompt(promptTexts.promptsUpgrade(this.current)).then(
-        function(props) {
+        function (props) {
           this.props = props;
           if (!props.upgrade) {
             process.exit(0);
@@ -136,8 +137,7 @@ module.exports = class extends Generator {
     this.widget = {};
     this.widget.widgetName = this.props.widgetName;
     this.widget.packageName = this.props.widgetName.toLowerCase();
-    this.widget.description =
-      this.props.description || this.current.description;
+    this.widget.description = this.props.description || this.current.description;
     this.widget.version = this.props.version;
     this.widget.author = this.props.author || this.current.author;
     this.widget.date = new Date().toLocaleDateString();
@@ -150,225 +150,179 @@ module.exports = class extends Generator {
     this.widget.builder = this.props.builder;
 
     if (this.isNew) {
-      const source = boilerPlatePath;
-      this.props.widgetOptionsObj = {};
-      if (this.props.boilerplate === "empty") {
-        for (let i = 0; i < this.props.widgetoptions.length; i++) {
-          this.props.widgetOptionsObj[this.props.widgetoptions[i]] = true;
-        }
-      }
+      const source = this.props.boilerplate === "badgeWidgetBoilerPlate" ? boilerPlatePath : emptyBoilerplatePath;
 
-      // TODO: Define all files to be copied and their paths in 1 array
+      // Define generic files to be copied
+      const genericFiles = [
+        { fileSource: "README.md", destination: "README.md" },
+        { fileSource: "xsd/widget.xsd", destination: "xsd/widget.xsd" }
+      ];
+
+      // Define widget files to be copied
+      const widgetFiles = [
+        {
+          fileSource: `${widgetSrcFolder}WidgetName.ts.ejs`,
+          destination: `${widgetSrcFolder}${this.widget.widgetName}.ts`
+        },
+        {
+          fileSource: `${widgetSrcFolder}WidgetNameContainer.ts.ejs`,
+          destination: `${widgetSrcFolder}${this.widget.widgetName}Container.ts`
+        },
+        { fileSource: `${widgetSrcFolder}Alert.ts.ejs`, destination: `${widgetSrcFolder}Alert.ts` },
+        { fileSource: "src/WidgetName.webmodeler.ts.ejs", destination: "src/" + this.widget.widgetName + ".webmodeler.ts" },
+        { fileSource: "src/ui/WidgetName.css", destination: "src/ui/" + this.widget.widgetName + ".css" },
+        { fileSource: "src/WidgetName.xml", destination: "src/" + this.widget.widgetName + ".xml" }
+      ];
 
       // Copy generic files
-      this.fs.copy(
-        this.templatePath(boilerPlatePath + "README.md"),
-        this.destinationPath("README.md")
-      );
-      this.fs.copy(
-        this.templatePath(source + "dist/MxTestProject/Test.mpr"),
-        this.destinationPath("dist/MxTestProject/Test.mpr")
-      );
-      this.fs.copy(
-        this.templatePath(boilerPlatePath + "tests/"),
-        this.destinationPath("tests/")
-      );
-      this.fs.copy(
-        this.templatePath(boilerPlatePath + "typings/"),
-        this.destinationPath("typings/")
-      );
-      this.fs.copy(
-        this.templatePath(boilerPlatePath + "xsd/widget.xsd"),
-        this.destinationPath("xsd/widget.xsd")
-      );
-
-      // Copy files based on WidgetName
-      this.fs.copy(
-        this.templatePath(source + "src/components/WidgetName.ts.ejs"),
-        this.destinationPath("src/components/" + this.widget.widgetName + ".ts"),
-        {
-          process: function(file) {
-            var fileText = file.toString();
-            fileText = fileText
-              .replace(/WidgetName/g, this.widget.widgetName)
-              .replace(/packageName/g, this.widget.packageName);
-            return fileText;
-          }.bind(this)
-        }
-      );
-
-      this.fs.copy(
-        this.templatePath(source + "src/components/WidgetNameContainer.ts.ejs"),
-        this.destinationPath("src/components/" + this.widget.widgetName + "Container.ts"),
-        {
-          process: function(file) {
-            var fileText = file.toString();
-            fileText = fileText
-              .replace(/WidgetName/g, this.widget.widgetName)
-              .replace(/packageName/g, this.widget.packageName);
-            return fileText;
-          }.bind(this)
-        }
-      );
-
-      this.fs.copy(
-        this.templatePath(source + "src/components/Alert.ts.ejs"),
-        this.destinationPath("src/components/Alert.ts"),
-        {
-          process: function(file) {
-            return file.toString();
-          }.bind(this)
-        }
-      );
-
-      // Copy unit tests based on WidgetName
-      if (this.widget.unitTests) {
+      genericFiles.forEach(file => {
         this.fs.copy(
-          this.templatePath(source + "src/components/__tests__/WidgetName.spec.ts.ejs"),
-          this.destinationPath("src/components/__tests__/" + this.widget.widgetName + ".spec.ts"),
+          this.templatePath(source + file.fileSource),
+          this.destinationPath(file.destination)
+        );
+      });
+
+      // Copy widget files based on WidgetName
+      widgetFiles.forEach(file => {
+        this.fs.copy(
+          this.templatePath(source + file.fileSource),
+          this.destinationPath(file.destination),
           {
-            process: function(file) {
+            process: function (file) {
               var fileText = file.toString();
-              fileText = fileText.replace(
-                /WidgetName/g,
-                this.widget.widgetName
-              );
+              fileText = fileText
+                .replace(/WidgetName/g, this.widget.widgetName)
+                .replace(/packageName/g, this.widget.packageName);
               return fileText;
             }.bind(this)
           }
         );
+      });
 
+      if (this.props.boilerplate !== "empty") {
+        // Copy MxTestProject
         this.fs.copy(
-          this.templatePath(source + "src/components/__tests__/Alert.spec.ts.ejs"),
-          this.destinationPath("src/components/__tests__/Alert.spec.ts"),
-          {
-            process: function(file) {
-              return file.toString();
-            }.bind(this)
-          }
+          this.templatePath(source + "dist/MxTestProject/Test.mpr"),
+          this.destinationPath("dist/MxTestProject/Test.mpr")
         );
 
+        // Copy tests folder
         this.fs.copy(
-          this.templatePath(source + "tests/remap.js.ejs"),
-          this.destinationPath("tests/remap.js"),
-          {
-            process: function(file) {
-              return file.toString();
-            }.bind(this)
-          }
+          this.templatePath(source + "tests/"),
+          this.destinationPath("tests/")
         );
+
+        // Copy tests folder
+        this.fs.copy(
+          this.templatePath(source + "typings/"),
+          this.destinationPath("typings/")
+        );
+
+        // Copy unit tests based on WidgetName
+        if (this.widget.unitTests) {
+          this.fs.copy(
+            this.templatePath(source + "src/components/__tests__/WidgetName.spec.ts.ejs"),
+            this.destinationPath("src/components/__tests__/" + this.widget.widgetName + ".spec.ts"),
+            {
+              process: function (file) {
+                var fileText = file.toString();
+                fileText = fileText.replace(
+                  /WidgetName/g,
+                  this.widget.widgetName
+                );
+                return fileText;
+              }.bind(this)
+            }
+          );
+
+          this.fs.copy(
+            this.templatePath(source + "src/components/__tests__/Alert.spec.ts.ejs"),
+            this.destinationPath("src/components/__tests__/Alert.spec.ts"),
+            {
+              process: function (file) {
+                return file.toString();
+              }.bind(this)
+            }
+          );
+
+          this.fs.copy(
+            this.templatePath(source + "tests/remap.js.ejs"),
+            this.destinationPath("tests/remap.js"),
+            {
+              process: function (file) {
+                return file.toString();
+              }.bind(this)
+            }
+          );
+        }
+
+        // Copy end-to-end tests based on WidgetName
+        if (this.widget.e2eTests) {
+          this.fs.copy(
+            this.templatePath(source + "typings/WidgetName.d.ts.ejs"),
+            this.destinationPath("typings/" + this.widget.widgetName + ".d.ts"),
+            {
+              process: function (file) {
+                var fileText = file.toString();
+                fileText = fileText.replace(/WidgetName/g, this.widget.widgetName);
+                return fileText;
+              }.bind(this)
+            }
+          );
+
+          this.fs.copy(
+            this.templatePath(source + "e2e/WidgetName.spec.ts.ejs"),
+            this.destinationPath("tests/e2e/" + this.widget.widgetName + ".spec.ts"),
+            {
+              process: function (file) {
+                var fileText = file.toString();
+                fileText = fileText.replace(
+                  /WidgetName/g,
+                  this.widget.widgetName
+                );
+                return fileText;
+              }.bind(this)
+            }
+          );
+
+          this.fs.copy(
+            this.templatePath(source + "e2e/pages/home.page.ts.ejs"),
+            this.destinationPath("tests/e2e/pages/home.page.ts"),
+            {
+              process: function (file) {
+                return file.toString();
+              }.bind(this)
+            }
+          );
+
+          this.fs.copy(
+            this.templatePath(source + "e2e/wdio.conf.js.ejs"),
+            this.destinationPath("tests/e2e/wdio.conf.js"),
+            {
+              process: function (file) {
+                return file.toString();
+              }.bind(this)
+            }
+          );
+
+          this.fs.copy(
+            this.templatePath(source + "e2e/tsconfig.json"),
+            this.destinationPath("tests/e2e/tsconfig.json"),
+            {
+              process: function (file) {
+                return file.toString();
+              }.bind(this)
+            }
+          );
+        }
       }
-
-      // Copy end-to-end tests based on WidgetName
-      if (this.widget.e2eTests) {
-        this.fs.copy(
-          this.templatePath(source + "e2e/WidgetName.spec.ts.ejs"),
-          this.destinationPath("tests/e2e/" + this.widget.widgetName + ".spec.ts"),
-          {
-            process: function(file) {
-              var fileText = file.toString();
-              fileText = fileText.replace(
-                /WidgetName/g,
-                this.widget.widgetName
-              );
-              return fileText;
-            }.bind(this)
-          }
-        );
-
-        this.fs.copy(
-          this.templatePath(source + "e2e/pages/home.page.ts.ejs"),
-          this.destinationPath("tests/e2e/pages/home.page.ts"),
-          {
-            process: function(file) {
-              return file.toString();
-            }.bind(this)
-          }
-        );
-
-        this.fs.copy(
-          this.templatePath(source + "e2e/wdio.conf.js.ejs"),
-          this.destinationPath("tests/e2e/wdio.conf.js"),
-          {
-            process: function(file) {
-              return file.toString();
-            }.bind(this)
-          }
-        );
-
-        this.fs.copy(
-          this.templatePath(source + "e2e/tsconfig.json"),
-          this.destinationPath("tests/e2e/tsconfig.json"),
-          {
-            process: function(file) {
-              return file.toString();
-            }.bind(this)
-          }
-        );
-      }
-
-      this.fs.copy(
-        this.templatePath(source + "typings/WidgetName.d.ts.ejs"),
-        this.destinationPath("typings/" + this.widget.widgetName + ".d.ts"),
-        {
-          process: function(file) {
-            var fileText = file.toString();
-            fileText = fileText.replace(/WidgetName/g, this.widget.widgetName);
-            return fileText;
-          }.bind(this)
-        }
-      );
-
-      // Copy css based on WidgetName
-      this.fs.copy(
-        this.templatePath(source + "src/ui/WidgetName.css"),
-        this.destinationPath("src/ui/" + this.widget.widgetName + ".css"),
-        {
-          process: function(file) {
-            var fileText = file.toString();
-            fileText = fileText.replace(
-              /packageName/g,
-              this.widget.packageName
-            );
-            return fileText;
-          }.bind(this)
-        }
-      );
-
-      // Rename references in widget main ts
-      this.fs.copy(
-        this.templatePath(source + "src/components/WidgetName.ts.ejs"),
-        this.destinationPath("src/components/" + this.widget.widgetName + ".ts"),
-        {
-          process: function(file) {
-            var fileText = file.toString();
-            fileText = fileText.replace(/WidgetName/g, this.widget.widgetName);
-            return fileText;
-          }.bind(this)
-        }
-      );
-
-      // Rename references in widget webmodeler ts
-      this.fs.copy(
-        this.templatePath(source + "src/WidgetName.webmodeler.ts.ejs"),
-        this.destinationPath("src/" + this.widget.widgetName + ".webmodeler.ts"),
-        {
-          process: function(file) {
-            var fileText = file.toString();
-            fileText = fileText
-              .replace(/WidgetName/g, this.widget.widgetName)
-              .replace(/packageName/g, this.widget.packageName);
-            return fileText;
-          }.bind(this)
-        }
-      );
 
       // Rename references package.xml
       this.fs.copy(
         this.templatePath(source + "src/package.xml"),
         this.destinationPath("src/package.xml"),
         {
-          process: function(file) {
+          process: function (file) {
             let fileText = file.toString();
             fileText = fileText
               .replace(/WidgetName/g, this.widget.widgetName)
@@ -379,20 +333,6 @@ module.exports = class extends Generator {
         }
       );
 
-      // Rename references WidgetName
-      this.fs.copy(
-        this.templatePath(source + "src/WidgetName.xml"),
-        this.destinationPath("src/" + this.widget.widgetName + ".xml"),
-        {
-          process: function(file) {
-            var fileText = file.toString();
-            fileText = fileText
-              .replace(/WidgetName/g, this.widget.widgetName)
-              .replace(/packageName/g, this.widget.packageName);
-            return fileText;
-          }.bind(this)
-        }
-      );
     }
 
     // Gitignore
@@ -408,7 +348,7 @@ module.exports = class extends Generator {
       this.templatePath("webpack.config.js"),
       this.destinationPath("webpack.config.js"),
       {
-        process: function(file) {
+        process: function (file) {
           var fileText = file.toString();
           fileText = fileText.replace(/WidgetName/g, this.widget.widgetName);
           return fileText;
@@ -419,19 +359,19 @@ module.exports = class extends Generator {
     // Package.JSON
     try {
       extfs.removeSync(this.destinationPath("package.json"));
-    } catch (e) {}
+    } catch (e) { }
 
     this.fs.copyTpl(this.templatePath("_package.json"), this.destinationPath("package.json"), this.widget, {});
 
     // Add Gulp/Grunt/tsconfig/tslint/webpack/karma
     this.pkg = pkg;
 
-    try { extfs.removeSync(this.destinationPath("Gruntfile.js")); } catch (e) {}
-    try { extfs.removeSync(this.destinationPath("Gulpfile.js")); } catch (e) {}
-    try {  extfs.removeSync(this.destinationPath("tsconfig.json")); } catch (e) {}
-    try { extfs.removeSync(this.destinationPath("tslint.json")); } catch (e) {}
-    try { extfs.removeSync(this.destinationPath("karma.conf.js")); } catch (e) {}
-    try { extfs.removeSync(this.destinationPath("webpack.config.js")); } catch (e) {}
+    try { extfs.removeSync(this.destinationPath("Gruntfile.js")); } catch (e) { }
+    try { extfs.removeSync(this.destinationPath("Gulpfile.js")); } catch (e) { }
+    try { extfs.removeSync(this.destinationPath("tsconfig.json")); } catch (e) { }
+    try { extfs.removeSync(this.destinationPath("tslint.json")); } catch (e) { }
+    try { extfs.removeSync(this.destinationPath("karma.conf.js")); } catch (e) { }
+    try { extfs.removeSync(this.destinationPath("webpack.config.js")); } catch (e) { }
 
     if (this.widget.builder === "gulp") {
       this.fs.copyTpl(this.templatePath("Gulpfile.js"), this.destinationPath("Gulpfile.js"), this, {});
--- generators/app/lib/prompttexts.js
+++ generators/app/lib/prompttexts.js
@@ -1,81 +1,99 @@
 /*jshint -W108,-W069*/
 "use strict";
-var semver = require('semver');
+var semver = require("semver");
 
 function promptsNew() {
   return [{
-    type: 'input',
-    name: 'widgetName',
+    type: "input",
+    name: "widgetName",
     validate: function (input) {
       if (/^([a-zA-Z]*)$/.test(input)) { return true; }
-      return 'Your widget can only contain letters (a-z & A-Z). Please provide a valid name';
+      return "Your widget can only contain letters (a-z & A-Z). Please provide a valid name";
     },
-    message: 'What is name of your widget?',
-    default: 'Badge'
+    message: "What is name of your widget?",
+    default: "Badge"
   },
   {
-    type: 'input',
-    name: 'description',
-    message: 'Enter a description for your widget',
-    default: 'Shows a value as a badge or a color label'
+    type: "input",
+    name: "description",
+    message: "Enter a description for your widget",
+    default: "Shows a value as a badge or a color label"
   },
   {
-    type: 'input',
-    name: 'copyright',
-    message: 'Add a copyright',
-    default: 'Flock of birds 2018',
+    type: "input",
+    name: "copyright",
+    message: "Add a copyright",
+    default: "FOB 2018",
     store: true
   },
   {
-    type: 'input',
-    name: 'license',
-    message: 'Add a license',
-    default: 'Apache 2',
+    type: "input",
+    name: "license",
+    message: "Add a license",
+    default: "Apache 2",
     store: true
   },
   {
-    type: 'input',
-    name: 'version',
+    type: "input",
+    name: "version",
     validate: function (input) {
-      if (semver.valid(input) && semver.satisfies(input, '>=1.0.0')) {
+      if (semver.valid(input) && semver.satisfies(input, ">=1.0.0")) {
         return true;
       }
-      return 'Your version needs to be formatted as x.x.x and starts at 1.0.0. Using 1.0.0';
+      return "Your version needs to be formatted as x.x.x and starts at 1.0.0. Using 1.0.0";
     },
-    message: 'Initial version',
-    default: '1.0.0'
+    message: "Initial version",
+    default: "1.0.0"
   },
   {
-    type: 'input',
-    name: 'author',
-    message: 'Author',
-    default: 'Joseph ',
+    type: "input",
+    name: "author",
+    message: "Author",
+    default: "Joseph",
     store: true
   },
   {
-    type: 'list',
-    name: 'builder',
-    message: 'Which task runner do you want to use for development?',
-    choices: [{
-        name: 'Gulp (recommended)',
-        value: 'gulp'
-      },{
-        name: 'Grunt',
-        value: 'grunt'
+    type: "list",
+    name: "builder",
+    message: "Which task runner do you want to use for development?",
+    choices: [
+      {
+        name: "Grunt (recommended)",
+        value: "grunt"
+      },
+      {
+        name: "Gulp",
+        value: "gulp"
       }
     ],
     default: 0
   },
   {
-    type: 'confirm',
-    name: 'unitTests',
-    message: 'Add unit tests for the widget ?',
+    type: "list",
+    name: "boilerplate",
+    message: "Which template do you want to use for the widget?",
+    choices: [
+      {
+        name: "BadgeWidgetBoilerplate (recommended for beginners)",
+        value: "badgeWidgetBoilerPlate"
+      },
+      {
+        name: "Empty widget (recommended for more experienced developers)",
+        value: "empty"
+      }
+    ],
+    store: true
+  },
+  {
+    type: "confirm",
+    name: "unitTests",
+    message: "Add unit tests for the widget ? (recommended for BadgeWidgetBoilerplate)",
     default: false
   },
   {
-    type: 'confirm',
-    name: 'e2eTests',
-    message: 'Add End-to-end tests for the widget ?',
+    type: "confirm",
+    name: "e2eTests",
+    message: "Add End-to-end tests for the widget ? (recommended for BadgeWidgetBoilerplate)",
     default: false
   }];
 }
