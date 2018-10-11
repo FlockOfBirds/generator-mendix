aac446113f52453f939016d6506c089b3ebadf99
Joseph D <j_seremba@live.com>
2018-10-11 11:31:51 +0300

    Change post-build cross-zip coded url

--- generators/app/templates/crosszip-config.js
+++ generators/app/templates/crosszip-config.js
@@ -1,6 +1,23 @@
-const zip = require("cross-zip");
+var zip = require("cross-zip");
+var fs = require("fs");
+var path = require("path");
+const package = require("./package");
+var dirPath = "./dist/MxTestProject/widgets/";
 
-const inPath = path.join(__dirname, "dist/tmp/src");
-const outPath = path.join(__dirname, "dist/MxTestProject/widgets/WidgetName.mpk");
+if (fs.existsSync(path.join(__dirname, dirPath + "test.js"))) {
+    fs.unlinkSync(path.join(__dirname, dirPath + "test.js"), function (err) {
+        if (err) throw err;
+    });
+}
 
-zip.zipSync(inPath, outPath);
+if (fs.existsSync(path.join(__dirname, dirPath + package.widgetName + ".mpk"))) {
+    fs.unlink(path.join(__dirname, dirPath + package.widgetName + ".mpk"), function (err) {
+        if (err) throw err;
+    });
+}
+
+
+const tmpPath = path.join(__dirname, "./dist/tmp/src");
+const outPath = path.join(__dirname, dirPath + package.widgetName + ".mpk");
+
+zip.zipSync(tmpPath, outPath);
